cefe40544e234067303f3b53975f70202adf2ece
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-11-10 21:25:42 -0300

    Criacao

---
+++ JogoDaVelha/src/game/player/Player.java
@@ -0,0 +1,17 @@
+package game.player;
+
+import java.util.Scanner;
+
+public class Player {
+	private Integer countPLays;
+	
+	public Player() {
+		
+	}
+	
+	private String play() {
+		Scanner sc = new Scanner(System.in);
+		countPLays+=1;
+		return sc.next();
+	}
+}
